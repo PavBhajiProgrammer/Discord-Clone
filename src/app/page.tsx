15bc5f4855df1a2c96f3121eed1e601d0592722d
pavbhajiprogrammer <pavbhajiprogrammer@gmail.com>
2024-01-09 19:54:54 +0530

    project setup completed

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+// import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button.js"
+
+
+export default function Home() {
+ render: return (
+
+  <div>
+    <p className="text-3xl font-bold text-indigo-500">
+     Hello Discord Clone
+    </p>
+
+    <Button variant="outline">Button</Button>
+  </div>
+    
+  )
+}
